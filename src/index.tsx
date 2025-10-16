d4006d9a20157e602ab319daa8a086b391b4d06b
Dooky <chamallow14@hotmail.fr>
2025-10-16 12:20:51 -0300

    BrowserRouter added

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 // Type assertion to ensure TypeScript knows this won't be null
 const rootElement = document.getElementById('root') as HTMLElement;
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(rootElement);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
